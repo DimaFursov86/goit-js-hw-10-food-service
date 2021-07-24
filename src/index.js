0018117c8c5ae7199c2a50646c52aa5e70d0a60c
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-07-24 15:29:33 +0300

    finished switch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import foodCardTpl from './js/jsmenu';
-console.log(foodCardTpl);
-
 import foodCards from './menu.json';
-
-
 
 const cardsList = document.querySelector(".js-menu");
 
@@ -17,44 +13,37 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
 const switchControl = document.querySelector(".theme-switch__control");
 const switchCheck = document.querySelector('#theme-switch-toggle');
-console.dir(switchCheck)
-switchControl.addEventListener("click", onSwitchControlClik);
+const bodyClassSelected = localStorage.getItem('bodyClasses');
+themeChange()
 
+switchControl.addEventListener("change", onSwitchControlClik);
 
-document.body.className = Theme.LIGHT;
-
-// localStorage.setItem('bodyClasses', JSON.stringify(Theme));
-//   const bodyClassSelected = JSON.parse(localStorage.getItem('bodyClasses'));
-function onSwitchControlClik(e) {
+function onSwitchControlClik() {
   
-  e.preventDefault();
-  
-  
-  let bodyClass = document.body.className;
-
-localStorage.setItem('bodyClasses', bodyClass);
-  const bodyClassSelected = localStorage.getItem('bodyClasses');
-  document.body.className = bodyClassSelected
-  
-
-  if (bodyClassSelected === Theme.LIGHT) {
-    (document.body.className = "dark-theme") && (switchCheck.checked = true);
+    if (switchCheck.checked) {
     
-  };
-
-  if (bodyClassSelected === Theme.DARK) {
-    (document.body.className = "light-theme") && (switchCheck.checked = false)
-  };
-  
-  
-  // if (bodyClass === bodyClassSelected.LIGHT || ' ') {
-  //   (document.body.className = "dark-theme") && (switchCheck.checked = true);
-  // }
-  // if (bodyClass === bodyClassSelected.DARK) {(document.body.className = "light-theme") && (switchCheck.checked = false);
-  // };
-  console.log(switchCheck.checked)
-  console.log(bodyClass);
-  
+       localStorage.setItem('bodyClasses', Theme.DARK);
+      
+       document.body.className = Theme.DARK;
+    }
+    else {
+    
+       localStorage.setItem('bodyClasses', Theme.LIGHT);
+      
+       document.body.className = Theme.LIGHT;
+    };
+ 
 }
+
+function themeChange() {
+ 
+  if (bodyClassSelected === Theme.DARK) {
+    switchCheck.checked = true;
+    document.body.className = bodyClassSelected;
+  }
+  else {document.body.className = bodyClassSelected};
+};
+
