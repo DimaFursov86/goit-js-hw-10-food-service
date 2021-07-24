75880c23f436631acec5c3f8158a4aa7c7afa946
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-07-24 09:37:46 +0300

    try1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ console.log(foodCardTpl);
 import foodCards from './menu.json';
 
 
-console.log(foodCards);
+
 const cardsList = document.querySelector(".js-menu");
 
 cardsList.insertAdjacentHTML("beforeend", createFoodCardsMarkup(foodCards))
@@ -12,3 +12,49 @@ cardsList.insertAdjacentHTML("beforeend", createFoodCardsMarkup(foodCards))
   
       return foodCardTpl(foodCards);
 };
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const switchControl = document.querySelector(".theme-switch__control");
+const switchCheck = document.querySelector('#theme-switch-toggle');
+console.dir(switchCheck)
+switchControl.addEventListener("click", onSwitchControlClik);
+
+
+document.body.className = Theme.LIGHT;
+
+// localStorage.setItem('bodyClasses', JSON.stringify(Theme));
+//   const bodyClassSelected = JSON.parse(localStorage.getItem('bodyClasses'));
+function onSwitchControlClik(e) {
+  
+  e.preventDefault();
+  
+  
+  let bodyClass = document.body.className;
+
+localStorage.setItem('bodyClasses', bodyClass);
+  const bodyClassSelected = localStorage.getItem('bodyClasses');
+  document.body.className = bodyClassSelected
+  
+
+  if (bodyClassSelected === Theme.LIGHT) {
+    (document.body.className = "dark-theme") && (switchCheck.checked = true);
+    
+  };
+
+  if (bodyClassSelected === Theme.DARK) {
+    (document.body.className = "light-theme") && (switchCheck.checked = false)
+  };
+  
+  
+  // if (bodyClass === bodyClassSelected.LIGHT || ' ') {
+  //   (document.body.className = "dark-theme") && (switchCheck.checked = true);
+  // }
+  // if (bodyClass === bodyClassSelected.DARK) {(document.body.className = "light-theme") && (switchCheck.checked = false);
+  // };
+  console.log(switchCheck.checked)
+  console.log(bodyClass);
+  
+}
